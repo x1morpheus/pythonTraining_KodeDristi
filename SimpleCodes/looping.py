4a1f37d3435cb73ee06ab076211d22a3164f6424
x1morpheus <prayush917@gmail.com>
2026-02-06 20:00:53 +0545

    Looping codes

---
+++ SimpleCodes/looping.py
@@ -0,0 +1,15 @@
+fruits = ["papaya", "mango", "banana", "grapes" ]
+
+for fruit in fruits:
+    print(fruit)
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+i = 10
+while i>=0:
+    print(i)
+    i -= 1
